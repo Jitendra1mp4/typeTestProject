9e1559c2b1ee77bcd502da80a7c659ee0f667d35
jitendra1mp4 <jitendrasahu6761@gmail.com>
2021-10-20 17:23:15 +0530

    Charecter counting features are added

--- Scripts/script.js
+++ Scripts/script.js
@@ -12,25 +12,35 @@ function countWord() {
       invisibel()
    }
 }
-
+function numberOfWord(sentence) {
+   if (sentence == null) return 0;
+   let arr = sentence.trim().split(" ");
+   console.log(arr)
+   return arr.length;
+}
 function visible() {
+   let text_type = document.getElementById('text_type').value;
+   let chars = document.getElementById('chars');
    let btn_analyze = document.getElementById('btn_analyze');
    let result = document.getElementById('result');
-   let speed = document.getElementById('speed') ;
-   let rightWords = document.getElementById('rightWords') ;
-   let wrongWords = document.getElementById('wrongWords') ;
+   let speed = document.getElementById('speed');
+   let rightWords = document.getElementById('rightWords');
+   let wrongWords = document.getElementById('wrongWords');
    console.log("odd");
    result.style.visibility = "visible";
    btn_analyze.value = "Retry";
-   speed.value = 10 ;
-   rightWords.value = 8 ;
-   wrongWords.value = speed.value - rightWords.value ;
+   speed.value = numberOfWord(text_type);
+   rightWords.value = 0;
+   wrongWords.value = speed.value - rightWords.value;
+   chars.value = text_type.length;
 
 }
 function invisibel() {
+   let text_type = document.getElementById('text_type');
    let btn_analyze = document.getElementById('btn_analyze');
    let result = document.getElementById('result');
    console.log("Even");
    result.style.visibility = "collapse";
+   text_type.value = null;
    btn_analyze.value = "Analyze my skill";
 }
